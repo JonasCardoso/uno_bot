6a0892ebe0ebf3aa475879edf21c309a71044785
Deni Putra Perdana <deni.putra@bukalapak.com>
2018-10-22 18:51:58 +0700

    simple_commands.py: win percentage

--- simple_commands.py
+++ simple_commands.py
@@ -133,6 +133,13 @@ def stats(bot, update):
               "{number} first places",
               n).format(number=n)
         )
+        
+        n = (us.first_places / us.games_played) * 100
+        stats_text.append(
+            _("{number}% win percentage",
+              "{number}% win percentage",
+              n).format(number=round(n,2))
+        )
 
         n = us.last_places
         stats_text.append(
