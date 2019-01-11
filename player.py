d46ceb10e9779a40b7afc13d28e09a8c9c8a2125
Jonas Cardoso <jonascard60@gmail.com>
2019-01-11 13:33:11 -0200

    player: Remove verification of can't play +4 as last card

--- player.py
+++ player.py
@@ -151,10 +151,6 @@ class Player(object):
 
                 self.bluffing = (self.bluffing or card.color == last.color)
 
-        # You may not play a chooser or +4 as your last card
-        if len(self.cards) == 1 and self.cards[0].special:
-            return list()
-
         return playable
 
     def _card_playable(self, card):
